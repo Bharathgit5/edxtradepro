87132119374895c870b362e3e96ae73d2377f67f
Bharathgit5 <119423513+Bharathgit5@users.noreply.github.com>
2025-09-18 17:16:26 +0530

    Update SignUp.jsx

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -24,6 +24,7 @@ export default function SignUp() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
+         credentials: 'include', 
       });
       const data = await res.json();
       console.log(data);
