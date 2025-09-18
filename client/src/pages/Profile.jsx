6327b5af9a1d803afb3a46a0966ce9fcdbacb692
Bharathgit5 <119423513+Bharathgit5@users.noreply.github.com>
2025-09-18 17:12:08 +0530

    Update Profile.jsx

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -77,6 +77,7 @@ export default function Profile() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
+         credentials: 'include'
       });
       const data = await res.json();
       if (data.success === false) {
@@ -96,6 +97,7 @@ export default function Profile() {
       dispatch(deleteUserStart());
      const res = await fetch(`${API_BASE}/user/delete/${currentUser._id}`, {
         method: 'DELETE',
+         credentials: 'include'
       });
       const data = await res.json();
       if (data.success === false) {
@@ -111,7 +113,10 @@ export default function Profile() {
   const handleSignOut = async () => {
     try {
       dispatch(signOutUserStart());
-       const res = await fetch(`${API_BASE}/auth/signout`);
+   const res = await fetch(`${API_BASE}/auth/signout`, {
+  credentials: 'include'
+});
+
       const data = await res.json();
       if (data.success === false) {
         dispatch(deleteUserFailure(data.message));
@@ -126,7 +131,10 @@ export default function Profile() {
   const handleShowListings = async () => {
     try {
       setShowListingsError(false);
-       const res = await fetch(`${API_BASE}/user/listings/${currentUser._id}`)
+     const res = await fetch(`${API_BASE}/user/listings/${currentUser._id}`, {
+  credentials: 'include'
+});
+
      
    
       const data = await res.json();
@@ -145,6 +153,7 @@ export default function Profile() {
     try {
        const res = await fetch(`${API_BASE}/listing/delete/${listingId}`, {
         method: 'DELETE',
+          credentials: 'include'
       });
       const data = await res.json();
       if (data.success === false) {
