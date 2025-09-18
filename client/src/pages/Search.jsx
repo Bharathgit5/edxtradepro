683411bb61410e26c2f05c63e70081c04718885c
Bharathgit5 <119423513+Bharathgit5@users.noreply.github.com>
2025-09-18 17:13:38 +0530

    Update Search.jsx

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -48,7 +48,9 @@ export default function Search() {
       setLoading(true);
       setShowMore(false);
       const searchQuery = urlParams.toString();
-      const res = await fetch(`${API_BASE}/listing/get?${searchQuery}`);
+      const res = await fetch(`${API_BASE}/listing/get?${searchQuery}`, {
+  credentials: 'include'
+});
       const data = await res.json();
       if (data.length > 8) {
         setShowMore(true);
@@ -113,7 +115,9 @@ export default function Search() {
     const urlParams = new URLSearchParams(location.search);
     urlParams.set('startIndex', startIndex);
     const searchQuery = urlParams.toString();
-    const res = await fetch(`${API_BASE}/listing/get?₹{searchQuery}`);
+    const res = await fetch(`${API_BASE}/listing/get?₹{searchQuery}`, {
+  credentials: 'include'
+});
     const data = await res.json();
     if (data.length < 9) {
       setShowMore(false);
